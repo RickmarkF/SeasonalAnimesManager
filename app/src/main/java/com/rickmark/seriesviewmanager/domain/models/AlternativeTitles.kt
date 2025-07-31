306a7dcf810278331b6b0e77ef5593df3f3ed979
Rickmark <rickmark2000@outlook.com>
2025-07-31 21:06:27 +0200

    anime details information showed

--- app/src/main/java/com/rickmark/seriesviewmanager/domain/models/AlternativeTitles.kt
+++ app/src/main/java/com/rickmark/seriesviewmanager/domain/models/AlternativeTitles.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AlternativeTitles(
-    val synonyms: List<String>,
+    val synonyms: ArrayList<String>,
 
     @SerialName("en")
     val english: String,
--- app/src/main/java/com/rickmark/seriesviewmanager/ui/seasonalAnime/DetailSeasonalAnimeFragment.kt
+++ app/src/main/java/com/rickmark/seriesviewmanager/ui/seasonalAnime/DetailSeasonalAnimeFragment.kt
@@ -1,9 +1,15 @@
 package com.rickmark.seriesviewmanager.ui.seasonalAnime
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.rickmark.seriesviewmanager.R
 import com.rickmark.seriesviewmanager.data.request.AnimeManager
 import com.rickmark.seriesviewmanager.domain.interfaces.IAnimeManager
@@ -19,6 +25,22 @@ class DetailSeasonalAnimeFragment : Fragment(R.layout.fragment_detail_seasonal_a
 
         val manager: IAnimeManager = AnimeManager()
         val data: AnimeDetails? = manager.getAnimeDetails(animeId)
+
+        val animeImage: ImageView = view.findViewById(R.id.imageSeasonalAnime)
+        val sinopsys: TextView = view.findViewById(R.id.sinopsysSeasonalAnime)
+        val alternativeTittlesSpinner: Spinner = view.findViewById(R.id.alternativeTittlesSpinner)
+
+        val tittleList: ArrayList<String> = data?.alternativeTitles!!.synonyms
+        tittleList.add(data?.alternativeTitles!!.english)
+        tittleList.add(data?.alternativeTitles!!.japanese)
+
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter(view.context, android.R.layout.simple_spinner_item,
+                tittleList)
+        alternativeTittlesSpinner.adapter = adapter
+        Glide.with(this).load(data?.mainPicture?.large).into(animeImage);
+        sinopsys.text = data?.synopsis
+
 
 
     }
