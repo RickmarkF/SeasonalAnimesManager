613388580e0524ca019797263cb44cdf0be66ac8
Rickmark <rickmark2000@outlook.com>
2025-08-02 15:39:50 +0200

    problems with theme solved

---
+++ app/src/main/java/com/rickmark/seriesviewmanager/ui/MyProfileFragment.kt
@@ -0,0 +1,16 @@
+package com.rickmark.seriesviewmanager.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rickmark.seriesviewmanager.R
+
+class MyProfileFragment : Fragment(R.layout.fragment_my_profile) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
